4805976d9409bf0e20e42a3b070420c8057f97d2
anthonylife <anthonylife@anthonylife.(none)>
2013-09-23 12:58:15 +0800

    new file:   dataprep/segment_rawdata.py

---
+++ dataprep/segment_rawdata.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#encoding=utf8
+
+'''
+@author: Wei Zhang
+@date: 2013/9/23
+@description: file "segment_rawdata.py" segments the raw file into different
+              parts accroding to functions.
+'''
+
+
+import json
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
