22c3e11161d7a7f4806da5285e9c25b846a2ba9f
anthonylife <anthonylife@anthonylife.(none)>
2013-10-08 11:45:55 +0800

    modified:   Readme.txt 	modified:   filter_others.py

--- preprocess_script/Readme.txt
+++ preprocess_script/Readme.txt
@@ -2,3 +2,17 @@ Data Preprocessing
 ====================
 This directory contains all codes preprocessing raw data and converting them
 into the format that our model code can directly use.
+
+Steps:
+======
+1. Extract data from the original file (e.g. /Doctor/Data/Reviews/...) and
+    output each to part of them to the separate files.
+    -->Related scripts<--:
+        segment_rawdata.py
+
+2. Filter users and items according to the frequency of users and items.
+    -->Related scripts<--:
+        filter_user_item.py --> output user id and item id to the files
+                                in the directory "data/filter"
+        filter_others.py --> extract (1) time, (2) score, (3) text according to
+                                the filtered user/item list.
--- preprocess_script/filter_others.py
+++ preprocess_script/filter_others.py
@@ -8,18 +8,106 @@
     to the filtered users and items list.
 '''
 
-import json
+import csv, json
+
+def filter_line(infile, saved_set):
+    outputlines = []
+    for line in open(infile):
+        line = line.strip("\n\t\r")
+
+        bi_items = line.split(",")
+        if bi_items[0] in saved_set:
+            outputlines.append(bi_items)
+    return outputlines
+
+def filter_data(old_time_file, old_score_file, old_text_file, old_reviewid_file,
+        user_file, item_file, new_time_file, new_score_file, new_text_file,
+        new_reviewid_file):
+    user_set = set([user[0] for user in csv.reader(open(user_file))])
+    item_set = set([item[0] for item in csv.reader(open(item_file))])
+
+    reviewid_set = set([])
+    outputlines = []
+    writer = csv.writer(open(new_reviewid_file, "w"), lineterminator='\n')
+    for line in csv.reader(open(old_reviewid_file)):
+        userid = line[0]
+        if userid in user_set:
+            outputline = [userid]
+            for item in line[1:]:
+                itemid = item.split(" ")[0]
+                reviewid = item.split(" ")[1]
+                if itemid in item_set:
+                    reviewid_set.add(reviewid)
+                    outputline.append(item)
+            outputlines.append(outputline)
+    writer.writerows(outputlines)
+
+    writer = csv.writer(open(new_time_file, "w"), lineterminator="\n")
+    outputlines = filter_line(old_time_file, reviewid_set)
+    writer.writerows(outputlines)
+
+    writer = csv.writer(open(new_score_file, "w"), lineterminator="\n")
+    outputlines = filter_line(old_score_file, reviewid_set)
+    writer.writerows(outputlines)
+
+    writer = csv.writer(open(new_text_file, "w"), lineterminator="\n")
+    outputlines = filter_line(old_text_file, reviewid_set)
+    writer.writerows(outputlines)
 
 def main():
     print 'Loading files paths...'
     paths = json.loads(open("SETTINGS.json").read())
 
     print 'Start filtering in CellarTracker (1)time, (2)score, (3)text according'
-    reviewid_time_file = paths["clean_data_dir1"] + paths["reviewid_time_file"]
-    reviewid_score_file = paths["clean_data_dir1"] + paths["reviewid_score_file"]
-    reviewid_text_file = paths["clean_data_dir1"] + paths["reviewid_text_file"]
+    old_time_file = paths["clean_data_dir1"] + paths["reviewid_time_file"]
+    old_score_file = paths["clean_data_dir1"] + paths["reviewid_score_file"]
+    old_text_file = paths["clean_data_dir1"] + paths["reviewid_text_file"]
+    old_user_item_review_file = paths["clean_data_dir1"] + paths["user_item_review_file"]
     filtered_user_file = paths["filter_data_dir1"] + paths["raw_filter_user_file"]
-    filtered_item_file =
+    filtered_item_file = paths["filter_data_dir1"] + paths["raw_filter_item_file"]
+    new_time_file = paths["filter_data_dir1"] + paths["reviewid_time_file"]
+    new_score_file = paths["filter_data_dir1"] + paths["reviewid_score_file"]
+    new_text_file = paths["filter_data_dir1"] + paths["reviewid_text_file"]
+    new_user_item_review_file = paths["filter_data_dir1"] + paths["user_item_review_file"]
+    filter_data(old_time_file, old_score_file, old_text_file,
+            old_user_item_review_file, filtered_user_file, filtered_item_file,
+            new_time_file, new_score_file, new_text_file, new_user_item_review_file)
+    print 'Finish filtering!'
+    raw_input()
+
+    print 'Start filtering in Amazon Moview (1)time, (2)score, (3)text according'
+    old_time_file = paths["clean_data_dir2"] + paths["reviewid_time_file"]
+    old_score_file = paths["clean_data_dir2"] + paths["reviewid_score_file"]
+    old_text_file = paths["clean_data_dir2"] + paths["reviewid_text_file"]
+    old_user_item_review_file = paths["clean_data_dir2"] + paths["user_item_review_file"]
+    filtered_user_file = paths["filter_data_dir2"] + paths["raw_filter_user_file"]
+    filtered_item_file = paths["filter_data_dir2"] + paths["raw_filter_item_file"]
+    new_time_file = paths["filter_data_dir2"] + paths["reviewid_time_file"]
+    new_score_file = paths["filter_data_dir2"] + paths["reviewid_score_file"]
+    new_text_file = paths["filter_data_dir2"] + paths["reviewid_text_file"]
+    new_user_item_review_file = paths["filter_data_dir2"] + paths["user_item_review_file"]
+    filter_data(old_time_file, old_score_file, old_text_file,
+            old_user_item_review_file, filtered_user_file, filtered_item_file,
+            new_time_file, new_score_file, new_text_file, new_user_item_review_file)
+    print 'Finish filtering!'
+    raw_input()
+
+    print 'Start filtering in Amazon Food (1)time, (2)score, (3)text according'
+    old_time_file = paths["clean_data_dir3"] + paths["reviewid_time_file"]
+    old_score_file = paths["clean_data_dir3"] + paths["reviewid_score_file"]
+    old_text_file = paths["clean_data_dir3"] + paths["reviewid_text_file"]
+    old_user_item_review_file = paths["clean_data_dir3"] + paths["user_item_review_file"]
+    filtered_user_file = paths["filter_data_dir3"] + paths["raw_filter_user_file"]
+    filtered_item_file = paths["filter_data_dir3"] + paths["raw_filter_item_file"]
+    new_time_file = paths["filter_data_dir3"] + paths["reviewid_time_file"]
+    new_score_file = paths["filter_data_dir3"] + paths["reviewid_score_file"]
+    new_text_file = paths["filter_data_dir3"] + paths["reviewid_text_file"]
+    new_user_item_review_file = paths["filter_data_dir3"] + paths["user_item_review_file"]
+    filter_data(old_time_file, old_score_file, old_text_file,
+            old_user_item_review_file, filtered_user_file, filtered_item_file,
+            new_time_file, new_score_file, new_text_file, new_user_item_review_file)
+    print 'Finish filtering!'
+
 
 if __name__ == "__main__":
     main()
